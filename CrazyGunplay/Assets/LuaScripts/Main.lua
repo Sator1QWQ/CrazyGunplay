82263f842d1d6ecd1f518118a03952209c538210
satori <2399049698@qq.com>
2023-11-25 10:59:52 +0800

    增加定义

--- CrazyGunplay/Assets/LuaScripts/Main.lua
+++ CrazyGunplay/Assets/LuaScripts/Main.lua
@@ -2,6 +2,10 @@ UnityEngine = CS.UnityEngine
 Debug = UnityEngine.Debug
 UI = UnityEngine.UI
 Vector3 = UnityEngine.Vector3
+EventSystems = UnityEngine.EventSystems
+EventTrigger = EventSystems.EventTrigger
+
+UI = CS.Module.UI
 
 Text = require "Global.Text"
 
