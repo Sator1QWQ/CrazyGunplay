8719a307981a50ec30466388532d67d269daaf98
satori <2399049698@qq.com>
2023-12-04 09:20:55 +0800

    角色配置表

---
+++ CrazyGunplay/Assets/LuaScripts/Configs/Config/Character.lua
@@ -0,0 +1,19 @@
+require "Configs.Text.Character_Text"
+Character = {
+	[1001] = {
+		id = 1001,
+		name = Character_Text.name_4,
+		initWeapon = 1001,
+		skillGroup = 1001,
+		model = Character_Text.model_4,
+		desc = Character_Text.desc_4,
+	},
+	[1002] = {
+		id = 1002,
+		name = Character_Text.name_5,
+		initWeapon = 1001,
+		skillGroup = 1001,
+		model = Character_Text.model_5,
+		desc = Character_Text.desc_5,
+	},
+}
---
+++ CrazyGunplay/Assets/LuaScripts/Configs/Config/CharacterData.lua
@@ -0,0 +1,19 @@
+require "Configs.Text.CharacterData_Text"
+CharacterData = {
+	[1001] = {
+		id = 1001,
+		speed = 1.2,
+		jump = 0.8,
+		airJumpNum = 1,
+		dushDistance = 1.8,
+		dushNum = 1,
+	},
+	[1002] = {
+		id = 1002,
+		speed = 1.2,
+		jump = 0.8,
+		airJumpNum = 1,
+		dushDistance = 1.8,
+		dushNum = 1,
+	},
+}
