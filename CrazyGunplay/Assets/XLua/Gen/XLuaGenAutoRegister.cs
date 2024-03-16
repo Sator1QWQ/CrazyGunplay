f7c088a8925e201d1047c38cf290affd585b4021
satori <2399049698@qq.com>
2024-03-16 11:32:31 +0800

    xlua生成

--- CrazyGunplay/Assets/XLua/Gen/XLuaGenAutoRegister.cs
+++ CrazyGunplay/Assets/XLua/Gen/XLuaGenAutoRegister.cs
@@ -22,6 +22,9 @@ namespace XLua.CSObjectWrap
         static void wrapInit0(LuaEnv luaenv, ObjectTranslator translator)
         {
         
+            translator.DelayWrapLoader(typeof(SceneTool), SceneToolWrap.__Register);
+        
+        
             translator.DelayWrapLoader(typeof(object), SystemObjectWrap.__Register);
         
         
