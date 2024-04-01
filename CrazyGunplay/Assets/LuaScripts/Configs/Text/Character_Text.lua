486e4b5771da3e01c447831476cae2a117f0a5fc
satori <2399049698@qq.com>
2024-04-01 15:55:45 +0800

    角色配置

--- CrazyGunplay/Assets/LuaScripts/Configs/Text/Character_Text.lua
+++ CrazyGunplay/Assets/LuaScripts/Configs/Text/Character_Text.lua
@@ -1,7 +1,7 @@
 Character_Text = {}
 Character_Text.name_4 = "幽影"
-Character_Text.model_4 = "aaaa"
+Character_Text.model_4 = "SA_Character_Biohazard"
 Character_Text.desc_4 = "这是一个角色1"
 Character_Text.name_5 = "qqqq"
-Character_Text.model_5 = "aaaa"
+Character_Text.model_5 = "SA_Character_BusinessSurvivor"
 Character_Text.desc_5 = "wwww"
