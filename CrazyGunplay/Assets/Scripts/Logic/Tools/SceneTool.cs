64437224b5bf57cf885aad4993d21398ee76da3c
satori <2399049698@qq.com>
2024-04-09 16:27:43 +0800

    场景工具修改

--- CrazyGunplay/Assets/Scripts/Logic/Tools/SceneTool.cs
+++ CrazyGunplay/Assets/Scripts/Logic/Tools/SceneTool.cs
@@ -17,7 +17,37 @@ public class SceneTool
     public static void ChangeScene(int sceneId)
     {
         string sceneName = Config.Get<string>("Scene", sceneId, "assetPath");
+        RemoveSceneAssets();
+        string path = GlobalDefine.SCENE_PATH + sceneName;
+        Module.Scene.LoadScene(path);
+    }
 
+    /// <summary>
+    /// 除了Start场景，其他的场景都卸载
+    /// </summary>
+    public static void UnloadAllScene()
+    {
+        //卸载场景
+        string[] loadedScene = Module.Scene.GetLoadedSceneAssetNames();
+        for (int i = 0; i < loadedScene.Length; i++)
+        {
+            Module.Scene.UnloadScene(loadedScene[i]);
+        }
+
+        //卸载ui
+        Module.UI.CloseAllLoadingUIForms();
+        Module.UI.CloseAllLoadedUIForms();
+
+        //卸载实体
+        Module.Entity.HideAllLoadingEntities();
+        Module.Entity.HideAllLoadedEntities();
+
+        Module.Timer.RemoveAllTimer();
+    }
+
+    //清除场景资源
+    private static void RemoveSceneAssets()
+    {
         //卸载ui
         Module.UI.CloseAllLoadingUIForms();
         Module.UI.CloseAllLoadedUIForms();
@@ -30,22 +60,6 @@ public class SceneTool
 
         //卸载场景
         string[] loadedScene = Module.Scene.GetUnloadingSceneAssetNames();
-        for(int i = 0; i < loadedScene.Length; i++)
-        {
-            Module.Scene.UnloadScene(loadedScene[i]);
-        }
-
-        string path = GlobalDefine.SCENE_PATH + sceneName;
-        Module.Scene.LoadScene(path);
-    }
-
-    /// <summary>
-    /// 除了Start场景，其他的场景都卸载
-    /// </summary>
-    public static void UnloadAllScene()
-    {
-        //卸载场景
-        string[] loadedScene = Module.Scene.GetLoadedSceneAssetNames();
         for (int i = 0; i < loadedScene.Length; i++)
         {
             Module.Scene.UnloadScene(loadedScene[i]);
