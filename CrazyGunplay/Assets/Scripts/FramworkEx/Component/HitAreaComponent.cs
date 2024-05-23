3807196383d2caf2448a4dbc91987a3bdda4d8c7
satori <2399049698@qq.com>
2024-05-23 11:37:25 +0800

    命中检测添加仅检测功能

--- CrazyGunplay/Assets/Scripts/FramworkEx/Component/HitAreaComponent.cs
+++ CrazyGunplay/Assets/Scripts/FramworkEx/Component/HitAreaComponent.cs
@@ -10,7 +10,7 @@ using UnityGameFramework.Runtime;
 public class HitAreaComponent : GameFrameworkComponent
 {
     /// <summary>
-    /// 命中玩家判断，不一定是伤害命中，也可以是其他的回血等命中
+    /// 执行命中操作，不一定是伤害命中，也可以是其他的回血等命中
     /// </summary>
     /// <param name="owner">玩家</param>
     /// <param name="targetId">目标表id</param>
@@ -18,26 +18,7 @@ public class HitAreaComponent : GameFrameworkComponent
     /// <param name="startPoint"></param>
     public void HitPlayerAction(PlayerEntity owner, int targetId, int areaId, Vector3 startPoint, HitData data)
     {
-        Config_HitArea areaConfig = Config<Config_HitArea>.Get("HitArea", areaId);
-        List<PlayerEntity> entitys = Module.Target.FindTarget(owner, targetId);
-        List<PlayerEntity> result = new List<PlayerEntity>();
-        
-        //根据找到的目标进一步判断
-        for(int i = 0; i < entitys.Count; i++)
-        {
-            PlayerEntity entity = entitys[i];
-            bool isFind = false;
-            if(areaConfig.areaType == AreaType.Circle)
-            {
-                isFind = CheckByCircle(entity.Entity.transform, startPoint, areaConfig.value[0]);
-            }
-            
-            if(isFind)
-            {
-                result.Add(entity);
-            }
-        }
-
+        List<PlayerEntity> result = GetHitResult(owner, targetId, areaId, startPoint, data);
         for (int i = 0; i < result.Count; i++)
         {
             HitData newData = new HitData();
@@ -48,6 +29,47 @@ public class HitAreaComponent : GameFrameworkComponent
             HitEventArgs args = HitEventArgs.Create(newData);
             Module.Event.FireNow(this, args);
         }
+    }
+
+    /// <summary>
+    /// 仅检测是否命中玩家，不会发送广播
+    /// </summary>
+    /// <param name="owner"></param>
+    /// <param name="targetId"></param>
+    /// <param name="areaId"></param>
+    /// <param name="startPoint"></param>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    public bool IsHitPlayer(PlayerEntity owner, int targetId, int areaId, Vector3 startPoint, HitData data)
+    {
+        List<PlayerEntity> result = GetHitResult(owner, targetId, areaId, startPoint, data);
+        return result.Count > 0;
+    }
+
+    //命中检测
+    private List<PlayerEntity> GetHitResult(PlayerEntity owner, int targetId, int areaId, Vector3 startPoint, HitData data)
+    {
+        Config_HitArea areaConfig = Config<Config_HitArea>.Get("HitArea", areaId);
+        List<PlayerEntity> entitys = Module.Target.FindTarget(owner, targetId);
+        List<PlayerEntity> result = new List<PlayerEntity>();
+
+        //根据找到的目标进一步判断
+        for (int i = 0; i < entitys.Count; i++)
+        {
+            PlayerEntity entity = entitys[i];
+            bool isFind = false;
+            if (areaConfig.areaType == AreaType.Circle)
+            {
+                isFind = CheckByCircle(entity.Entity.transform, startPoint, areaConfig.value[0]);
+            }
+
+            if (isFind)
+            {
+                result.Add(entity);
+            }
+        }
+
+        return result;
     }
 
     private bool CheckByCircle(Transform findEntity, Vector3 startPoint, float radius)
